c3e1c4f98f228113bcad85cabff707132783545b
double d <brunduk4415@gmail.com>
2023-07-15 22:24:52 +0300

    update

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -13,7 +13,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(amount);
         int expected = 0;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
         System.out.println(service.remain(amount));
     }
 
@@ -26,7 +26,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(amount);
         int expected = 100;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
         System.out.println(service.remain(amount));
     }
 
@@ -39,7 +39,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(amount);
         int expected = 1000;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
         System.out.println(service.remain(amount));
     }
 }
