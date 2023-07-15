65464c6d6b47f099ecdc05d9a3056f19cb03b6fb
double d <brunduk4415@gmail.com>
2023-07-15 21:18:22 +0300

    Work done junit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,45 @@
+package ru.netology.service;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @org.junit.Test
+    public void CashbackBuyEqualsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+        System.out.println(service.remain(amount));
+    }
+
+    @org.junit.Test
+    public void CashbackBuy() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+        System.out.println(service.remain(amount));
+    }
+
+    @org.junit.Test
+    public void CashbackBuyZero() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+        System.out.println(service.remain(amount));
+    }
+}
